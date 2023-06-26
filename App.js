4e2968de301203677b53c1a31fc6ec293a2f3ede
Mansoor-Ahmed-Butt <creativedesigner921@gmail.com>
2023-06-26 12:02:49 -0700

    My project is going to start

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function App() {
+  return (
+    <View>
+      <Text>React Native with Git_Hub</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
